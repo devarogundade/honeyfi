47ef7460d8770c4ab2d92d9c1c724f5c12350a6b
devarogundade <devarogundade@gmail.com>
2024-09-19 12:59:47 +0100

    update

--- ui/src/router/index.ts
+++ ui/src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
+import HomeView from '@/views/HomeView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +12,12 @@ const router = createRouter({
     {
       path: '/pools',
       name: 'pools',
-      component: () => import('../views/PoolsView.vue')
+      component: () => import('@/views/PoolsView.vue')
+    },
+    {
+      path: '/hive',
+      name: 'hive',
+      component: () => import('@/views/HiveView.vue')
     }
   ]
 });
---
+++ ui/src/views/HiveView.vue
@@ -0,0 +1,60 @@
+<template>
+    <section>
+        <div class="app_width">
+            <div class="hive_view">
+                <p class="text">
+                    In the Hive of Golden Trade
+                    <br> <br>
+                    In the hive where bees do dwell,
+                    They buzz and hum, their stories tell.
+                    With wings of light, they search and roam,
+                    Bringing nectar sweet back home.
+                    <br> <br>
+                    Each bee a worker, swift and true,
+                    Building riches as they flew.
+                    From flower to flower, they glean and dive,
+                    Just like swaps within the hive.
+                    <br> <br>
+                    The honey pot, a treasure bright,
+                    Is earned with every soaring flight.
+                    For testnet users, bees of gold,
+                    Rewards await for swaps untold.
+                    <br> <br>
+                    The hive’s a network, vast and wide,
+                    With pathways clear and honey to guide.
+                    Each token earned, a golden gleam,
+                    A swap’s sweet nectar, a dream redeemed.
+                    <br> <br>
+                    So buzz, dear bee, and do your trade,
+                    In HoneyFi’s hive, the work is laid.
+                    For those who swap with care and glee,
+                    The sweetest rewards are theirs to see.
+                </p>
+            </div>
+        </div>
+    </section>
+</template>
+
+<style scoped>
+.hive_view {
+    padding-top: 40px;
+    padding-bottom: 80px;
+    display: flex;
+    justify-content: center;
+}
+
+.text {
+    width: 500px;
+    max-width: 100%;
+    color: var(--tx-dimmed);
+    font-size: 16px;
+    line-height: 24px;
+}
+
+@media screen and (max-width: 1000px) {
+    .hive_view {
+        padding-top: 36px;
+        padding-bottom: 100px;
+    }
+}
+</style>
